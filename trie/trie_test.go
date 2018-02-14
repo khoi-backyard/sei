d0e9d90cd0048298a8f9a670e19b21e8886b53b5
Khoi Lai <khoi.geeky@gmail.com>
2018-02-14 18:19:53 +0700

    Add tests for trie

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -2,20 +2,40 @@ package trie
 
 import "testing"
 
+var tests = []struct {
+	route string
+}{
+	{"/api/v1/"},
+	{"/api/v1/products"},
+	{"/api/v1/products/1"},
+}
+
 func TestTrie_Add(t *testing.T) {
 	trie := New()
 
-	key := "/abc"
+	data := "some associated data"
+	n := trie.Add("/api/v1", data)
 
-	n := trie.Add(key, 123)
+	if n.Data().(string) != data {
+		t.Errorf("Expected %s, got: %s", data, n.Data().(string))
+	}
+}
 
-	found, ok := trie.Find(key)
-
-	if !ok {
-		t.Fatal("Can't find %s", key)
+func TestTrie_Find(t *testing.T) {
+	trie := New()
+	for _, c := range tests {
+		trie.Add(c.route, c.route)
 	}
 
-	if found.Data().(int) != 123 {
-		t.Errorf("Expected 123, got: %d", n.Data().(int))
+	for _, c := range tests {
+		n, ok := trie.Find(c.route)
+
+		if !ok {
+			t.Errorf("Expected to find %s", c.route)
+		}
+
+		if n.Data().(string) != c.route {
+			t.Errorf("Expected %s, got: %s", c.route, n.Data().(string))
+		}
 	}
 }
